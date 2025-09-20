01b0315b2bcede6a0d2b22caae129f003c0a94be
Hnioua <abdessamadhnioua@gmail.com>
2025-09-20 18:55:24 +0100

    up1

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,17 +5,16 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
+  base: "./", // ✅ important pour les assets en prod
   server: {
-    port: "4028",
+    port: 4028,
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-  }
+    // allowedHosts supprimé, car inutile / bloque Vercel
+  },
 });
